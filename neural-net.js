d24336b5f34d08435568c8f7180da417275f2f7d
John Fulton <jfulton@squareup.com>
2019-02-25 19:49:51 +1100

    allow arbitrary hidden layer structure

--- neural-net.js
+++ neural-net.js
@@ -2,7 +2,8 @@ const sigmoid = num => 1/(1+Math.exp((-num) / 1));
 
 
 const INPUT_LAYER_SIZE = 2;
-const HIDDEN_LAYER_SIZE = 2
+//const HIDDEN_LAYER_SIZE = 2
+const HIDDEN_LAYER_SIZES = [2]
 const OUTPUT_LAYER_SIZE = 1;
 
 
@@ -47,11 +48,24 @@ const createRandomNode = (inputSize) => new Node(
 // A Neural network is by default constructed using random nodes
 class NeuralNetwork {
   constructor() {
+    let inputLayer = [..._.times(INPUT_LAYER_SIZE, () => new InputNode()), new BiasNode()];
+    let lastLayerSize = INPUT_LAYER_SIZE + 1;
+    let hiddenLayers = HIDDEN_LAYER_SIZES.map((layerSize) => {
+      return [..._.times(layerSize, () => createRandomNode(lastLayerSize)), new BiasNode()]
+      lastLayerSize = layerSize + 1;
+    });
+    let outputLayer = _.times(OUTPUT_LAYER_SIZE, () => createRandomNode(lastLayerSize));
+
     this.layers = [
-      [..._.range(INPUT_LAYER_SIZE).map(() => new InputNode()), new BiasNode()],
-      [..._.range(HIDDEN_LAYER_SIZE).map(() => createRandomNode(INPUT_LAYER_SIZE)), new BiasNode()],
-      _.range(OUTPUT_LAYER_SIZE).map(() => createRandomNode(HIDDEN_LAYER_SIZE)),
-    ];
+      inputLayer,
+      ...hiddenLayers,
+      outputLayer
+    ]
+    //this.layers = [
+    //  [..._.range(INPUT_LAYER_SIZE).map(() => new InputNode()), new BiasNode()],
+    //  [..._.range(HIDDEN_LAYER_SIZE).map(() => createRandomNode(INPUT_LAYER_SIZE)), new BiasNode()],
+    //  _.range(OUTPUT_LAYER_SIZE).map(() => createRandomNode(HIDDEN_LAYER_SIZE)),
+    //];
   }
 
   // takes an array of inputs, returns an array of outputs
